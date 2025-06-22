599b10c244d678dca73ff6f4b63ad014356c57ba
skfatima-codes <skaneezfatima188@gmail.com>
2025-06-22 20:17:06 +0500

    Correction of code

--- Datatype.py
+++ Datatype.py
@@ -2,4 +2,4 @@ a=1 # a is a integar
 b=4.6 # b is a floating point number
 c="FAtima"# fatima is a string
 d=True or False #  d boolean conditions
-e= none # e none is datatype (means iskay andar kuch bhi nhi hai )
+  # e none is datatype (means iskay andar kuch bhi nhi hai )
